89fe50843c8317144c8feb834aa463bed74266da
sqz777 <sqz777isme@gmail.com>
2018-01-04 21:32:31 +0800

    加入專案檔案。

---
+++ LeetCode_NumberOfDigitOne/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode_NumberOfDigitOne
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
