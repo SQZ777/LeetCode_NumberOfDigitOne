c5bb83cad6fe2db07c880a8018abc0f7a7b1f6de
sqz777 <sqz777isme@gmail.com>
2018-01-04 22:22:36 +0800

    fail HasDigitOne test case : input 0

--- LeetCode_NumberOfDigitOne/UnitTest1.cs
+++ LeetCode_NumberOfDigitOne/UnitTest1.cs
@@ -6,8 +6,17 @@ namespace LeetCode_NumberOfDigitOne
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void HasDigitOne_Input_0_Should_Be_false()
         {
+            Assert.IsFalse(Solution.HasDigitOne(0));
+        }
+    }
+
+    public class Solution
+    {
+        public static bool HasDigitOne(int i)
+        {
+            throw new System.NotImplementedException();
         }
     }
 }
