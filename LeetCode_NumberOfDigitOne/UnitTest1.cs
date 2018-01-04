ec1847bf1ec8914d94cc910b3b579377b1aa66d3
sqz777 <sqz777isme@gmail.com>
2018-01-04 22:25:02 +0800

    pass HasDigitOne test case : input 1

--- LeetCode_NumberOfDigitOne/UnitTest1.cs
+++ LeetCode_NumberOfDigitOne/UnitTest1.cs
@@ -10,12 +10,22 @@ namespace LeetCode_NumberOfDigitOne
         {
             Assert.IsFalse(Solution.HasDigitOne(0));
         }
+
+        [TestMethod]
+        public void HasDigitOne_Input_1_Should_Be_True()
+        {
+            Assert.IsTrue(Solution.HasDigitOne(1));
+        }
     }
 
     public class Solution
     {
         public static bool HasDigitOne(int i)
         {
+            if (i.ToString().Contains("1"))
+            {
+                return true;
+            }
             return false;
         }
     }
